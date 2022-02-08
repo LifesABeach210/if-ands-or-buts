597c67fc5b3af2190bb5796f113aa91b7c0a0092
Daniel Rothwell <danielrothwell@Daniels-MacBook-Pro.local>
2022-02-08 18:02:43 -0500

    hello

---
+++ bill.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+
+let total = Number(prompt('Enter bill total: '));
+let Person1 = Number(prompt('Amount Person 1 paid: '));
+let Person2 = Number(prompt('Amount Person 2 paid: '));
+
